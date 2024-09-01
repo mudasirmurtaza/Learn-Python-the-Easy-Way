4ec8f9dad3cfceb5903721d236684b4aa86ccc4c
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-09-01 11:01:04 +0500

    Create 2_Variable.py
    The second topic of our learning journey is variables in Python.
    
    Variables in Python are containers used to store data values. They are like labels or names that you assign to specific values. When you use a variable, you're essentially referring to the value it holds.

---
+++ 2_Variable.py
@@ -0,0 +1,17 @@
+#Variable is basically a name given to a memory location in a program
+#implict is where we don't tell type there like age=23 is okay and explicit is like int age 21
+
+name = "Mudasir"
+age = 21
+price = 20.99
+
+print (age)   
+print (name)
+print (price)
+print (age, price, name)
+
+print ("My name is :", name)
+print ("My age is:", age)
+
+age2=age
+print (age2)
