0c4f7cfe0f476cad5cce2d64fe1a89e35b29f1e9
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-09-01 10:52:01 +0500

    Create 1_Print.py
    This is the first program of our Python learning journey.

---
+++ 1_Print.py
@@ -0,0 +1,6 @@
+print ("Hello World")   #print is basically a function in python and there purpose is to complete any sort of task assigned to it.
+print ("Trying to learn Pyhton")
+print ("I am 21 years old", "I am currently a 6th sem Computer Science Student")
+print (23+23)
+print (23)
+print (23-23)
