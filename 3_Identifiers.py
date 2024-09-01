432235d06d821ac13e2dd8cafbfc4b2e28d1a0c8
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-09-01 11:03:05 +0500

    Create 3_Identifiers.py
    Our third topic is Identifiers in Python.
    
    Identifiers in Python are names given to entities like variables, functions, classes, modules, etc. They serve as labels to uniquely identify these elements within your code.
    
    Rules for Identifiers:
    
    1. Must start with a letter (a-z, A-Z) or an underscore (_).
    2. Can contain letters, digits (0-9), and underscores.
    3. Cannot be a keyword. (Keywords are reserved words in Python with specific meanings, such as if, else, for, while, etc.)
    4. Case-sensitive. myVariable and myvariable are considered different identifiers.
    
    Examples of valid identifiers:
    myVariable
    _myPrivateVariable
    my_function
    MyClass
    
    Examples of invalid identifiers:
    123variable (starts with a digit)
    my-variable (contains a hyphen)
    if (is a keyword)

---
+++ 3_Identifiers.py
@@ -0,0 +1,14 @@
+"""
+#1.Can be combination of uppercase, lowercase, digits, underscore
+#2.Indentifier can not start with a digit
+#3 We can't use special symbols like !@#$%^&*
+#4.Indentifier can be of any length
+#5.A good name is short, simple and meangingful """
+
+name = "Mudasir"
+age = 21
+price = 20.99
+
+print (type(name))
+print (type(age))
+print (type(price))
