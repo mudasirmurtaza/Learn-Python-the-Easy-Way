f473259989ca1e0993b7852e82057996e683691a
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-09-01 11:05:49 +0500

    Create 5_DataTypes.py
    Our fourth topic is Data Types in Python.
    
    Data types in Python define the kind of values a variable can hold and the operations that can be performed on them. Python has a rich set of built-in data types, each with its own characteristics and uses. Here are some of the most common data types:
    
    Numerical Data Types:
    int: Represents integer numbers (e.g., 10, -5, 0)
    float: Represents floating-point numbers (e.g., 3.14, -2.5)
    complex: Represents complex numbers (e.g., 2+3j)
    
    Sequence Data Types:
    list: Ordered collection of elements, which can be of different data types (e.g., [1, "hello", 3.14])
    tuple: Immutable ordered collection of elements (e.g., (1, 2, 3))
    str: Sequence of characters (e.g., "Hello, world!")
    bytes: Sequence of bytes (e.g., b'abc')
    
    Mapping Data Type:
    dict: Unordered collection of key-value pairs (e.g., {'name': 'Alice', 'age': 30})
    
    Set Data Types:
    set: Unordered collection of unique elements (e.g., {1, 2, 3})
    frozenset: Immutable set (e.g., frozenset([1, 2, 3]))
    
    Boolean Data Type:
    bool: Represents True or False values (e.g., True, False)
    
    None Data Type:
    NoneType: Represents the absence of a value (e.g., None)

---
+++ 5_DataTypes.py
@@ -0,0 +1,24 @@
+# Python has 5 datatypes 
+#1. Integer    +ve.-ve,0
+#2. String     "Ali", "Hello"
+#3. Float       1.2, 1.3, 99.99
+#4. Boolean     True or False
+#5. None        where we don't want to assign nothing
+
+
+name = 'Mudasir', "Mudasir", "'Mudasir'"  #but double quotes are more common
+namee = "Mudasir"
+age = 21
+price = 20.99
+
+print (type(name))
+print (type(namee))
+print (type(age))
+print (type(price))
+
+age=23
+old=False
+a=None
+
+print(type(old))
+print(type(a))
