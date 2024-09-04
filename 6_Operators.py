5a2bfba309f83fca19425679bfc421483812f2bd
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-09-04 14:27:36 +0500

    Create 6_Operators.py

---
+++ 6_Operators.py
@@ -0,0 +1,13 @@
+""" 
+An operator is a symbol that performs a certain operation between operands.Apna College
+Types of Operators
+Arithmetic Operators ( + , - , * , / , % , ** )
+Relational / Comparison Operators ( == , != , > , < , >= , <= )
+Assignment Operators ( = , +=, -= , *= , /= , %= , **=  )
+Logical Operators ( not , and , or ) 
+Membership Operator (in, not in)
+Identity Operator (is, is not)
+Bitwise Operator (&, |.\, ^ )
+"""
+
+# ** is like power 2**2 is like 2 power 2
