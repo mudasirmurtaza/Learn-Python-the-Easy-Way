1a03afd1d0a3bfe26cf3c465e5fc457e538dda11
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-09-05 12:55:59 +0500

    Add files via upload

---
+++ 8 punctuators.py
@@ -0,0 +1,6 @@
+#Symbols to organize sentence structure in programming
+#ex: () {} [] @ #
+
+#Python is a implicit language where we don't need to specify the datatype always
+#Explicit language we have to specify the datatypes 
+
